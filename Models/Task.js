a05cd9fc71b4b541e2f77721e8ac9ffacf75b2e7
Sushma <101826833+Sushi1005@users.noreply.github.com>
2025-02-07 16:30:42 -0500

    Update Task.js

--- Models/Task.js
+++ Models/Task.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
     title: String, #new code
-    description: String,
+    description: Character,
     status: { type: String, enum: ['Pending', 'In Progress', 'Completed'], default: 'Pending' },
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 }, { timestamps: true });
